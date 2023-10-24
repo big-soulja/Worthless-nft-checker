f77cea212989d388b647a618bdabd870683106ac
Petr <sherikhovp@mail.ru>
2023-10-24 13:15:44 +0300

    Optimized alchemy api calls and added timer

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ const alchemy = new Alchemy(settings);
 const ownerAddr = "bigsoulja.eth";
 console.log("fetching NFTs for address:", ownerAddr);
 console.log("...");
-var notWorthlessNft = 0;
+let notWorthlessNft = 0;
+let cleared = 0;
 
 const getNftsPercentage = async () => {
+  let startTime = new Date();
   const nftsForOwner = await alchemy.nft.getNftsForOwner(ownerAddr);
   const totalNftCount = nftsForOwner.totalCount;
   console.log("number of NFTs found:", totalNftCount);
@@ -36,7 +38,7 @@ const getNftsPercentage = async () => {
         var floor = await alchemy.nft.getFloorPrice(nft.contract.address);
         var fp = floor.openSea.floorPrice;
         var sales = await alchemy.nft.getNftSales(queryParams);
-        console.log(Object.keys(sales).length);
+        //console.log(Object.keys(sales).length);
 
         if (
           fp !== undefined &&
@@ -52,6 +54,13 @@ const getNftsPercentage = async () => {
         }
       })()
     );
+    if (nftPromises.length >= 70) {
+      await Promise.all(nftPromises);
+      nftPromises.length = 0; // Clear the array
+      cleared += 70;
+      console.log(cleared);
+      // await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second
+    }
   }
 
   await Promise.all(nftPromises);
@@ -67,58 +76,11 @@ const getNftsPercentage = async () => {
       " of your nfts are worthless"
   );
   console.log("Total value = " + totalFp);
+  let endTime = new Date();
+  let elapsedTime = endTime - startTime;
+  console.log(`The program ran for ${elapsedTime} milliseconds.`);
 };
 
 getNftsPercentage();
 
 // clean up promise all and fix readability
-
-// Create a limiter that allows, for example, 5 requests per second
-// const limiter = new Bottleneck({
-//   reservoir: 100, // Maximum number of requests
-//   reservoirRefreshAmount: 100, // Number of requests to add back to the reservoir after waiting
-//   reservoirRefreshInterval: 2000, // Time interval to add back requests to the reservoir (in ms)
-// });
-
-// const getNftsPercentage = async () => {
-//   const nftsForOwner = await alchemy.nft.getNftsForOwner(ownerAddr);
-//   const totalNftCount = nftsForOwner.totalCount;
-//   console.log("number of NFTs found:", totalNftCount);
-//   console.log("...");
-//   var totalFp = 0;
-//   const nftPromises = [];
-
-//   for await (const nft of alchemy.nft.getNftsForOwnerIterator(ownerAddr)) {
-//     nftPromises.push(
-//       limiter.schedule(async () => {
-//         var floor = await alchemy.nft.getFloorPrice(nft.contract.address);
-//         var fp = floor.openSea.floorPrice;
-//         if (fp !== undefined) {
-//           console.log("==");
-//           console.log(fp);
-//           totalFp += fp;
-//           console.log(totalFp);
-//           if (fp < 0.01) {
-//             console.log("worthless");
-//             worthlessNft = worthlessNft + 1;
-//           }
-//         }
-//       })
-//     );
-//   }
-
-//   await Promise.all(nftPromises);
-
-//   console.log("===");
-//   console.log(worthlessNft);
-//   const worthlessPercentage = (worthlessNft / totalNftCount) * 100;
-//   console.log(
-//     "Only " +
-//       Math.ceil(worthlessPercentage) +
-//       "%" +
-//       " of your nfts are worthless"
-//   );
-//   console.log("Total value = " + totalFp);
-// };
-
-// getNftsPercentage();
